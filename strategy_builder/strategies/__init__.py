e3dda957745e612c3af3926a9657d127361b3dcb
Noufal <noufal85@gmail.com>
2025-06-27 02:38:26 -0700

    added more fixes

--- strategy_builder/strategies/__init__.py
+++ strategy_builder/strategies/__init__.py
@@ -2,16 +2,17 @@
 Sample strategies and usage examples for the Strategy Builder framework
 """
 
-from .moving_average_crossover import MovingAverageCrossover
-from .trend_following import TrendFollowingStrategy
+# Temporarily commenting out imports that cause dependency issues
+# from .moving_average_crossover import MovingAverageCrossover
+# from .trend_following import TrendFollowingStrategy
 from .run_backtest import run_ma_crossover_backtest
-from .run_alpaca_backtest import run_alpaca_backtest
-from .run_marketstack_backtest import run_marketstack_backtest
+# from .run_alpaca_backtest import run_alpaca_backtest
+# from .run_marketstack_backtest import run_marketstack_backtest
 
 __all__ = [
-    'MovingAverageCrossover',
-    'TrendFollowingStrategy',
+    # 'MovingAverageCrossover',
+    # 'TrendFollowingStrategy',
     'run_ma_crossover_backtest',
-    'run_alpaca_backtest',
-    'run_marketstack_backtest'
+    # 'run_alpaca_backtest',
+    # 'run_marketstack_backtest'
 ]
